c5172a300ea0eb6bfbe5141e1bcf2ddb9d001806
Daouda Fofana <daofof0@gmail.com>
2017-12-03 10:15:12 +0530

    Fixing error in mLab db link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 const db_user= 'DaoFof';
 const db_password = 'jetaimemaman1';
-const db_link_from_mLab = `mongodb://<${db_user}>:<${db_password}>@ds127436.mlab.com:27436/todo-app-api`;
+const db_link_from_mLab = `mongodb://${db_user}:${db_password}@ds127436.mlab.com:27436/todo-app-api`;
 
 mongoose.connect(db_link_from_mLab || 'mongodb://localhost:27017/TodoApp');
 
