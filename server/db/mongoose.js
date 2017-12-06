fd62c903f41e93a1f5b3880e0e0cbce9521b353e
Daouda Fofana <daofof0@gmail.com>
2017-12-06 11:30:47 +0530

    Fixing link of mLab db and local db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,6 @@ var mongoose =  require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const db_user= 'DaoFof';
-const db_password = 'jetaimemaman1';
-const db_link_from_mLab = `mongodb://${db_user}:${db_password}@ds127436.mlab.com:27436/todo-app-api`;
-
-mongoose.connect(db_link_from_mLab || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
