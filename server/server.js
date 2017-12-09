3f0c4e6bcfc37e88bd57cdc8a20a05167fc8e415
Daouda Fofana <daofof0@gmail.com>
2017-12-09 09:59:51 +0530

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ var app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json()); // return value from this json() is a function and it is the middleware that we want to pass to express
 
+
+
 app.post('/todos', (req, res) =>{
   var todo = new Todo({
     text: req.body.text
@@ -108,6 +110,18 @@ app.patch('/todos/:id', (req, res)=>{
 HTTP PATCH method is what we use when we are going to update a resource
 */
 
+app.post('/users',(req, res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+  var user =  new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+  }).then((token)=>{
+      res.header('x-auth', token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
 
 app.listen(port,()=>{
   console.log(`Started on port ${port}`);
