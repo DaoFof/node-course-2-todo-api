8d8fd2a78a5771b4a5afe3b0904454a577217630
Daouda Fofana <daofof0@gmail.com>
2017-12-17 01:35:52 +0530

    Add test for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,27 +4,11 @@ const {ObjectID} = require ('mongodb');
 
 const {app} =  require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos =[{
-  _id: new ObjectID(),
-  text: "First test todo"
-},{
-  _id: new ObjectID(),
-  text: "Second test todo",
-  completed: true,
-  completedAt : 333
-}];
-
-beforeEach((done)=>{
-  Todo.remove({}).then(()=>{
-    return Todo.insertMany(todos);
-    /*
-    insertMany() is used to insert many document cast in an array in a collection
-    */
-  }).then(()=>{
-    done();
-  });/*This is going to delete everything in the database*/
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 /*
 beforeEach() is a test lifecycle, is going to let us run some code before any test case, means if we have two test case, the first it() is going to be executed and then the beforeEach() will be rerun and then run the 2nd it.
 We are going to use it, to setup the database in a useful way. because below in the test suite,
@@ -184,3 +168,76 @@ describe('PATCH /todos/:id', ()=>{
       .end(done);
   });
 });
+
+describe('GET /users/me', ()=>{
+  it('should return user if authenticated', (done)=>{
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)//set() is used to set header in using supertest
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+  it('should return a 401 if not authenticated', (done)=>{
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res)=>{
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', ()=>{
+  it('should create a user', (done)=>{
+    var email = 'example@ddd.fr';
+    var password = '123aaa';
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res)=>{
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err)=>{
+        if(err){
+          return done(err);
+        }
+        User.findOne({email}).then((user)=>{
+          expect(user).toExist();
+          expect(user.password).toNotBe(password);
+          done();
+        });
+      });
+  });
+
+  it('should return validation errors if request invalid',(done)=>{
+    var email = 'dao.fr';
+    var password= 'aaa';
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create user if email in use', (done)=>{
+
+    request(app)
+      .post('/users')
+      .send({
+        email: users[0].email,
+        password: 'password123'
+      })
+      .expect(400)
+      .end(done);
+  });
+})
