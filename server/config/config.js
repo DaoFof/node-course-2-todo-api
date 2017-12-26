7756f7a2209d5c6398e5dc3abfa9b0abc73e07f3
Daouda Fofana <daofof0@gmail.com>
2017-12-27 04:49:53 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,17 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}else if(env === 'test'){
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if(env === 'development' || env ==='test'){
+  var config = require('./config.json');
+  /*
+  When we requre json it parse automatically into javascript object
+  */
+  var envConfig = config[env];// when we want to use variable to access a property we have to use bracket
+
+  Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+  });
+  /*
+  Object.keys(item) return an array of key of the item object
+  */
 }
